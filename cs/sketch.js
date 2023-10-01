683d31b1073ef20b87a5885744c4402c4cd041c3
ivy <ivyvodesign@gmail.com>
2023-10-01 23:53:44 +0100

    logo size

--- cs/sketch.js
+++ cs/sketch.js
@@ -7,3 +7,22 @@ document.addEventListener("DOMContentLoaded", function(){
       bsOffcanvas.toggle();
     });
   });
+
+
+   // Scroll to anchor and open accordion panel
+   document.querySelectorAll
+   ('a[data-bs-toggle="collapse"]').forEach(function (link) {
+       link.addEventListener('click', function (event) {
+           event.preventDefault();
+           let target = 
+               document.querySelector(this.getAttribute('href'));
+           let accordion = 
+               document.querySelector(this.getAttribute('data-bs-target'));
+
+           $('html, body').animate({
+               scrollTop: $(target).offset().top
+           }, 500);
+
+           $(accordion).collapse('show');
+       });
+   });
