df9b5c33fcde32698ea76446a38f7e243656a3d2
ivy <ivyvodesign@gmail.com>
2023-10-08 23:42:56 +0100

    changing md

--- sketch.js
+++ sketch.js
@@ -10,28 +10,27 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
    // Scroll to anchor and open accordion panel/ot working yet
-   document.querySelectorAll
-   ('a[data-bs-toggle="collapse"]').forEach(function (link) {
-       link.addEventListener('click', function (event) {
-           event.preventDefault();
-           let target = 
-               document.querySelector(this.getAttribute('href'));
-           let accordion = 
-               document.querySelector(this.getAttribute('data-bs-target'));
+//    document.querySelectorAll
+//    ('a[data-bs-toggle="collapse"]').forEach(function (link) {
+//        link.addEventListener('click', function (event) {
+//            event.preventDefault();
+//            let target = 
+//                document.querySelector(this.getAttribute('href'));
+//            let accordion = 
+//                document.querySelector(this.getAttribute('data-bs-target'));
 
-           $('html, body').animate({
-               scrollTop: $(target).offset().top
-           }, 500);
+//            $('html, body').animate({
+//                scrollTop: $(target).offset().top
+//            }, 500);
 
-           $(accordion).collapse('show');
-       });
-   });
+//            $(accordion).collapse('show');
+//        });
+//    });
 
 // click fullscreen
 let images = document.querySelectorAll('.fs');
-
 for (let i = 0; i < images.length; i++) {
-    images[i].ontouchstart = function() {
+    images[i].onclick = function() {
         if(images[i].fullscreenElement){ 
            images[i].exitFullscreen(); 
          } else { 
