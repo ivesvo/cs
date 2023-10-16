f1ac941e01e4670f5787c6128d5a28e88e3657c9
Nhan Phan <57086421+nhanphan0411@users.noreply.github.com>
2023-10-17 02:05:41 +0700

    deploy jekyll & re-layout lectures

---
+++ lectures/snippets/s1_window_stater.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(800, 500);
+  }
+  
+  function draw() {
+    background(50);
+    
+    noStroke();
+    fill(248, 255, 133);
+    ellipse(400, 250, 610, 410)
+    
+    fill(255, 133, 248)
+    ellipse(400, 250, 600, 400)
+  }
