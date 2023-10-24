d63d23b47d25a267e9eff4cdd2c8532d45649d47
Nhan Phan <57086421+nhanphan0411@users.noreply.github.com>
2023-10-24 18:31:43 +0700

    Adding code snippets

---
+++ lectures/snippets/s1_color.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(100, 100);
+}
+
+function draw() {
+    background(255, 48, 67); //Change background
+    fill(155, 0, 18); //fill the inside of the circle
+    noStroke(); //we don't want the border
+    circle(50, 50, 30);
+}
